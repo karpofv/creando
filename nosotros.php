bf52ce77cb24c370767441db0cc1d87dc22866c8
karpofv <karpofv.89@gmail.com>
2017-07-24 14:27:01 -0400

    version inicial creando
    asociacio civil creando

---
+++ nosotros.php
@@ -0,0 +1,55 @@
+<?php
+    include_once("includes/layout/headp.php");
+    include_once("includes/layout/sidebarp.php");
+?>
+    <div class="breadcrumb">
+        <h2>QUIENES SOMOS</h2> </div>
+    <div class="ourstory">
+        <div class="container">
+            <div class="col-md-10 col-md-offset-1"> <img src="img/9.jpg" alt="" class="img-responsive" />
+                <p class="parrafojusto"> La ASOCIACIÓN CIVIL CREANDO, fundada por Leomyr Rojas junto a un grupo de solidarios ciudadanos el 14 de mayo de 2014 en el estado Barinas, Municipio Barinas de la Parroquia Alto Barinas, con sede física en la Av. Venezuela. Reconocida como una institución con carácter esencialmente educativo, benéfico y social, La Asociación tiene por objeto exclusivo la planificación y ejecución de actividades culturales y comunitarias, para la promoción de la investigación científica de la realidad social y la difusión del pensamiento humanístico. Su activo principal es la fuente de conocimiento del personal, mano de obra calificada, dedicación y compromiso; los recursos económicos para su funcionamiento, provienen del aporte voluntario no solo de sus miembros, también pueden auto gestionar sus ingresos realizando actividades culturales, de formación académicas, publicaciones, de las subvenciones y aportes que reciben a través de convenios con personas o entidades, públicas o privadas, nacionales o extranjeras; así como también por cualesquier actividad, rifa, festival, donaciones, herencias o legados otorgados a la Asociación Civil y aceptados por ésta. La fundación CREANDO, además de enfrentar una realidad económica, presenta una realidad pública y humana; es una institución comunicante, por lo que todos sus miembros, socios y voluntarios interactúan de forma permanente haciéndose cada vez más indispensable el uso de modernas herramientas tecnológicas que permiten poner en práctica los planes estratégicos para la gestión de recursos en la organización, de tal forma que la misión, visión, objetivos y la política del talento humano se encuentren alineados tanto con las metas de la institución como con los principios que guían el proceso de toma de decisiones. En ese sentido, la organización sin fines de lucro CREANDO se define como figura independiente respecto a otras organizaciones privadas o públicas sin estar subordinada al gobierno o ser utilizadas como instrumento del estado, tienen características similares a otras instituciones privaras tales como el cumplimiento de objetivos, jerarquías, límites al interior y al exterior de la organización, regidas por estatutos legales bien definidos registrados ante las autoridades pertinentes según lo disponen las leyes de la República Bolivariana de Venezuela. </p>
+            </div>
+            <div class="col-md-6">
+                <div class="wow bounceIn" data-wow-offset="0" data-wow-delay="0.8s">
+                    <div class="services">
+                        <div class="icons"> <i class="fa fa-crosshairs fa-3x"></i> </div>
+                        <h4>Misión</h4>
+                        <p class="parrafojusto"> Contribuir al desarrollo sostenible del país, a través de tres áreas fundamentales, Social, Ecológica y Cultural, por medio de enseñanzas valiosas de gran contenido humano y acciones de cooperación, que conduzcan al bienestar de todas las persona CREANDO mayor conciencia sobre la importancia de dar formación integral al ciudadano de hoy. </p>
+                    </div>
+                </div>
+                <hr> </div>
+            <div class="col-md-6">
+                <div class="wow bounceIn" data-wow-offset="0" data-wow-delay="1.2s">
+                    <div class="services">
+                        <div class="icons"> <i class="fa fa-bullseye fa-3x"></i> </div>
+                        <h4>Visión</h4>
+                        <p class="parrafojusto"> Ser una institución promotora de una cultura superior, con gran capacidad para incidir en los cambios fundamentales que requiera nuestra sociedad y encaminar a toda persona en su Desarrollo Integral CREANDO una sociedad con mayor conciencia sobre causas sociales.</p>
+                    </div>
+                </div>
+                <hr> </div>
+            <div class="col-xs-12">
+                <div class="breadcrumb">
+                    <h2>OBJETIVOS</h2> </div>
+                <p style="text-justify:justify" class="parrafojusto">Promover el desarrollo económico para que las personas puedan responsabilizándose de sus acciones, fortaleciendo su comprensión política y social del entorno en el que viven, con proyectos muy ligados al acceso a los recursos y a la toma de decisiones de su comunidad. Estos objetivos los alcanza a través de la labor que desempeña en 4 áreas de trabajo: Económico, Social, Cultural y Espiritual.</p>
+                <div class="col-sm-4 bounceIn" data-wow-offset="0" data-wow-delay="1.2s">
+                    <li class="parrafojusto" style="min-height:150px;"><i class="fa fa-star
+"></i> Promover y difundir la ayuda social, el desarrollo económico, educativo, humano y cultural de las personas.</li>
+                    <li class="parrafojusto" style="min-height:150px;"><i class="fa fa-star
+"></i> Mantener de manera continua, programas de educación integral, en busca de la mejor calidad de vida de todas las personas, sosteniendo como principio fundamental, el ideal de que una mejor sociedad es la suma de los valores de sus integrantes.</li>
+                </div>
+                <div class="col-sm-4 bounceIn" data-wow-offset="0" data-wow-delay="1.2s">
+                    <li class="parrafojusto" style="min-height:150px;"><i class="fa fa-star
+"></i> Trabajar y colaborar con el estado y con instituciones nacionales e internacionales en la educación ambiental, la protección y defensa de los bosques, la fauna, las aguas, y el medio ambiente en general.</li>
+                    <li class="parrafojusto" style="min-height:150px;"><i class="fa fa-star
+"></i> Promover la cultura mediante la educación en las artes y las ciencias para así contribuir en el desarrollo del ser humano, capacitándolo para vivir en armonía con los demás y con su entorno.</li>
+                </div>
+                <div class="col-sm-4 bounceIn" data-wow-offset="0" data-wow-delay="1.2s">
+                    <li class="parrafojusto" style="min-height:150px;"><i class="fa fa-star
+"></i> Impulsar los valores más elevados y positivos del hombre, dando técnicas efectivas para lograr el abandono definitivo de todo tipo de vicios; promoviendo la cultura del alma y la educación de la conciencia para hacerse útil a la comunidad, a la naturaleza y a los fines del creador.</li>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    include_once("includes/layout/footp.php");
+?>
